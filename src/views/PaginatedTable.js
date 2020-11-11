db1eeefcb5b8ecd233f9ff35bb64d3992d2605c0
Nikos Chantzopoulos <nikoschantzopoulos@gmail.com>
2020-11-12 00:40:34 +0200

    Pagination table inside Container

--- src/views/PaginatedTable.js
+++ src/views/PaginatedTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Table } from 'reactstrap'
+import { Table } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 const PaginatedTable = (props) => {
--- src/views/RTable.js
+++ src/views/RTable.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { Alert, Input } from 'reactstrap'
+import {
+    Alert, Input, InputGroup, 
+    ButtonDropdown,
+    InputGroupAddon, Button, DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Container
+} from 'reactstrap'
 import PaginatedTable from './PaginatedTable'
 import axios from 'axios'
-import Pagination from './Pagination'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+// import {fa-hand-o-right} from '@fortawesome/free-solid-svg-icons'
+
+
 
 
 
@@ -16,6 +26,12 @@ const Rtable = () => {
     const [pages, setPages] = useState(1)
     const [currentPage, setCurrentPage] = useState(1)
     const [page, setPage] = useState(1)
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [splitButtonOpen, setSplitButtonOpen] = useState(false);
+    const toggleDropDown = () => setDropdownOpen(!dropdownOpen);
+    const toggleSplit = () => setSplitButtonOpen(!splitButtonOpen);
+    const [searchTerm, setSearchTerm] = useState("")
+
 
 
 
@@ -46,20 +62,31 @@ const Rtable = () => {
 
 
 
-    
+
     const nextPage = () => {
         setCurrentPage(currentPage + 1)
         console.log("Current page is  : " + currentPage)
-       
+
     }
 
     const prevPage = () => {
         setCurrentPage(currentPage - 1)
-        if(currentPage === 1) {
+        if (currentPage === 1) {
             setCurrentPage(1)
         }
         console.log("Current page is  : " + currentPage)
-    }  
+    }
+
+    const searchInvoices = (evn) => {
+        if (evn.keyCode === 13) {
+            console.log(evn.target.value)
+
+        }
+    }
+
+    const dropSelection = (event) => {
+        console.log(event)
+    }
 
 
     let component
@@ -69,10 +96,10 @@ const Rtable = () => {
         </Alert>
 
     }
-    if (error != '') {
+    if (error !== '') {
         component = <Alert color="danger">
-        <p>An error has occcured</p>
-    </Alert>
+            <p>An error has occcured</p>
+        </Alert>
     }
     else {
 
@@ -82,17 +109,36 @@ const Rtable = () => {
 
 
     return (
-        <div>
+        <Container>
+            <InputGroup>
+                {/* <InputGroupAddon addonType="prepend"><Button><FontAwesomeIcon icon={}></Button></InputGroupAddon>
+                <Input />
+                <InputGroupAddon addonType="append"><Button>I'm a button</Button></InputGroupAddon> */}
+                <ButtonDropdown addonType="append" isOpen={dropdownOpen} toggle={toggleDropDown} onClick={ dropSelection}>
+                    <DropdownToggle caret>
+                        Αναζήτηση
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem divider />
+                        <DropdownItem eventKey="AFM">ΑΦΜ</DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem >Όνομα</DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem >Αριθμό τιμολογίου</DropdownItem>
+                    </DropdownMenu>
+                </ButtonDropdown>
+                <Input />
+            </InputGroup>
             <div>
                 <ul className="pagination text-center">
-                    <li key={1} className="active"><button onClick={prevPage}>Previous</button></li>
-                    <li key={2} className="disabled"><Input bsSize="sm" value={page +"\\" + pages} readOnly={true}/></li>
+                    <li key={1} className="active"><button onClick={prevPage}>Prev</button></li>
+                    <li key={2} className="disabled"><Input bsSize="sm" value={page + "\\" + pages} readOnly={true} /></li>
                     <li key={3} className="active"><button onClick={nextPage}>Next</button></li>
+                    {/* <li><Input onKeyDown={searchInvoices} /></li> */}
                 </ul>
             </div>
             {component}
-        </div>
-    )
+            </Container>    )
 
 }
 
